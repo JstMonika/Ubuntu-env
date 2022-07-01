ea4ed755c489cf90cedd99ed83b65ae85da768ab
liuching <benjack30518@gmail.com>
2022-07-01 13:42:31 +0800

    env 0701e-ct

--- init.sh
+++ init.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+sudo apt update
+sudo apt upgrade
+
+sudo apt-get install zsh
+chsh -s $(which zsh)
+
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+
+sudo apt-get install python
+
 cd home
 cp ./.vimrc ~/.vimrc
 cp ./.zshrc ~/.zshrc
 cp ./.p10k.zsh ~/.p10k.zsh
+cp ./ct ~/ct
 
 cd
 git clone --recursive https://gitlab.com/archerindigo/vimrc.git
@@ -13,3 +24,9 @@ cp -Rv vimrc/.vim ~/.vim/
 git clone https://github.com/wting/autojump.git
 cd autojump
 python3 ./install.py
+
+sudo apt-get install build-essential
+
+cd
+ssh-keygen
+
