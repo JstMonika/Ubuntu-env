bcfc1f65f48f5adafab4fde2916fd3fe24122090
softund <benjack30518@gmail.com>
2024-09-24 17:23:46 +0800

    remove ct

--- init.sh
+++ init.sh
@@ -14,20 +14,22 @@ cd home
 cp ./.vimrc ~/.vimrc
 cp ./.zshrc ~/.zshrc
 cp ./.p10k.zsh ~/.p10k.zsh
-cp ./ct ~/ct
 
 cd
 git clone --recursive https://gitlab.com/archerindigo/vimrc.git
 cp vimrc/.vimrc ~
 cp -Rv vimrc/.vim ~/.vim/
 
-git clone https://github.com/wting/autojump.git
-cd autojump
-python3 ./install.py
-
 sudo apt-get install build-essential -y
 
 git config --global user.email "benjack30518@gmail.com"
 git config --global user.name "softund"
+git config core.editor "vim"
 
+git clone https://github.com/wting/autojump.git
+cd autojump
+python3 ./install.py
 
+cd ..
+rm -rf autojump
+rm -rf vimrc
